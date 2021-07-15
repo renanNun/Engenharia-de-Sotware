12227708b2960badf537cc1305856b0ad0ac423d
renanNun <renannunes400@gmail.com>
2021-07-15 18:37:14 -0300

    Adicionando nova fonte e adicionando scripts

--- Assets/Scripts/ConfigurationController.cs
+++ Assets/Scripts/ConfigurationController.cs
@@ -1,9 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class ControleConfiguracao : MonoBehaviour
 {
-    
+    private bool muted;
+
+    public AudioMixer audioMixer;
+
+    public Dropdown fontDropdown;
+
+    Font[] fontsTypes;
+
+    void start()
+    {
+        fontsTypes;
+
+        fontDropdown.clearOptions();
+
+        List<string> options = new List<string>();
+
+        fontDropdown.AddOptions(fontsTypes);
+    }
+
+    public void setVolume(float volume)
+    {
+        audioMixer.setFloat("volume",volume);
+    }
+
+    public void setFontSize(int fontSize)
+    {
+        
+    }
+
+    public void isMute(bool isMuted)
+    {
+        this->muted = isMuted;
+    }
 }
