e1c0d10d96e2b6e629378891ca9a06b2bd4f691f
Lucas <lrpessamilio@gmail.com>
2021-08-04 14:34:45 -0300

    Created base template for the plots

---
+++ Assets/Scripts/PlotManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlotManager : MonoBehaviour
+{
+    public List<RawImage> bars;
+    public Text titleText;
+
+    public void adjustPlotToData(List<float> data, string title)
+    {
+
+    }
+}
