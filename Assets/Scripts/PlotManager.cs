eaa806d75dba2cdc8124c2bf7b435fc3763c9310
LucasRibP <lrpessamilio@gmail.com>
2021-08-11 11:59:34 -0300

    Atualização dos labels do plot implementada

--- Assets/Scripts/PlotManager.cs
+++ Assets/Scripts/PlotManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class PlotManager : MonoBehaviour
 {
     public List<RectTransform> bars;
+    public List<Text> labels;
+    public List<RectTransform> labelRects;
     public Text titleText;
 
     public void adjustPlotToData(List<float> data, string title)
@@ -22,7 +24,10 @@ public class PlotManager : MonoBehaviour
         }
         for (int i = numBars; i > 0; i--)
         {
-            bars[bars.Count - i].localScale = new Vector3(1, data[data.Count - i] / max, 1);
+            float yScale = data[data.Count - i] / max;
+            bars[bars.Count - i].localScale = new Vector3(1, yScale, 1);
+            labels[bars.Count - i].text = (100*data[data.Count - i]).ToString() + "%";
+            labelRects[bars.Count - i].localPosition = new Vector3(0, yScale*370, 0);
         }
         titleText.text = title;
     }
