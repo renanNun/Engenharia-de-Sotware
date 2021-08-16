eec0766c6cd65570b2d6cb8d4f11e00b434bd64f
Victor Aquiles <victoraqalencar@gmail.com>
2021-08-16 20:02:40 -0300

    Refatorando o Singleton de Volume

--- Assets/Scripts/VolumeSingleton.cs
+++ Assets/Scripts/VolumeSingleton.cs
@@ -2,25 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VolumeSingleton : MonoBehaviour
+public class VolumeSingleton : Singleton
 {
-
-    static public VolumeSingleton instance; 
+ 
     static private float volume = 1F;
-    private void Awake()
-    {
-        if(instance != null)
-        {
-            Destroy(gameObject);
-        }
-
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-    }
-
+    
     public void setVolume(float value) {
         volume = value;
     }
