2ab1881e8296a0ef503cf1f30e356f552cfde27b
renanNun <renannunes400@gmail.com>
2021-08-29 10:16:42 -0300

    Teste de movimento de Cursor

--- Assets/Tests/TestQuizED.cs
+++ Assets/Tests/TestQuizED.cs
@@ -11,4 +11,18 @@ public class TestQuizED
     [SetUp]
     public void Setup() => SceneManager.LoadScene("QuizED");
 
+    [UnityTest]
+    public IEnumerable TestCursorMovement()
+    {
+        var cursor = GameObject.Find("Cursor");
+        var initialPos = cursor.transform.position;
+
+        var topButton = Object.FindObjectsOfType<Button>()[0];
+        topButton.onClick.Invoke();
+        var movedPos = cursor.transform.position;
+
+        Assert.AreNotEqual(initialPos, movedPos);
+        yield return null;
+    }
+
 }
