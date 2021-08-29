8f61f16c0405397b16f3c28c8d2b3cbe8ceb4caf
renanNun <renannunes400@gmail.com>
2021-08-29 10:12:26 -0300

    Criando classe de Testes do QuizED

---
+++ Assets/Tests/TestQuizED.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TestQuizED
+{
+    [SetUp]
+    public void Setup() => SceneManager.LoadScene("QuizED");
+
+}
